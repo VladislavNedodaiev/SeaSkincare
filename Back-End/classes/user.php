9f00c47176d11b6129e445c9d9a34cb8993b47c9
Vladislav Nedodaiev <vladislavnedodaiev@gmail.com>
2019-11-17 22:11:20 +0200

    Updated user class: +login function +logout function +constants +getUser function +register function

--- Back-End/classes/user.php
+++ Back-End/classes/user.php
@@ -2,63 +2,163 @@
 
 class user {
 	
-	var $user_id;
-	var $hash;
-	var $nickname;
-	var $email;
-	var $verification;	
+	public $user_id;
+	public $nickname;
+	public $email;
 	
-	function login(email, password) {
+	public const UNVERIFIED = "UNVERIFIED_USER";
+	public const NO_USER = "NO_USER";
+	public const EMAIL_REGISTERED = "EMAIL_REGISTERED";
+	public const NICKNAME_REGISTERED = "NICKNAME_REGISTERED";
+	
+	public const SUCCESS = "SUCCESS";
+	public const DB_ERROR = "DB_ERROR";
+	public const EMAIL_UNSENT = "EMAIL_UNSENT";
+	
+	// logging in (getting private data, such as email)
+	public function login($email, $password) {
 		
-		require "../connect_db.php";
+		require "env.php";
+		$mysqli = include DB_CONNECT;
 		
-		if ($mysqli->connect_errno) {
-			return false;
-		}
+		if (!$mysqli)
+			return self::DB_ERROR;
 		
-		if ($result = $mysqli->query("SELECT `user`.* FROM `user` WHERE `user`.`user_id`=".$this->account_id." AND `account`.`verified`=1;")) {
+		if ($result = $mysqli->query("SELECT `user`.* FROM `user` WHERE `user`.`email`='".$email."';")) {
 			if ($res = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-				$this->set_from_qresult($res);
-				return true;
+				if (!$res['verification'])
+					return self::UNVERIFIED;
+				if (password_verify($password, $res['hash'])) {
+
+					$this->logout();
+					
+					$this->user_id = $res['user_id'];
+					$this->nickname = $res['nickname'];
+					$this->email = $res['email'];
+					
+					return self::SUCCESS;
+					
+				}
 			}
 		}
 		
-		return false;
+		return self::NO_USER;
 		
 	}
 	
-	function register(email, password, nickname) {
-		
-		require "../connect_db.php";
-		
-	}
+	// logging out / making object empty
+	public function logout() {
 	
-	function verify(verification) {
-	
-		require "../connect_db.php";
+		$user_id = $nickname = $email = null;
 	
 	}
 	
-	// user id or nickname
-	function getUser(userData) {
+	// registering user
+	public function register($email, $password, $nickname) {
+		
+		require "env.php";
+		$mysqli = include DB_CONNECT;
+		
+		if (!$mysqli)
+			return self::DB_ERROR;
+		
+		if ($result = $mysqli->query("SELECT `user`.* FROM `user` WHERE `user`.`email`='".$email."' OR `user`.`nickname`='".$nickname."';")) {
+			if ($res = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+				if ($email == $res['email'])
+					return self::EMAIL_REGISTERED;
+				else
+					return self::NICKNAME_REGISTERED;
+			}
+		}
+		
+		// generating verification hash
+		$verification = md5(rand(0, 10000));
+		
+		$mysqli->query("START TRANSACTION;");
+		$mysqli->query("SAVEPOINT reg_".$nickname.";");
+		
+		if ($mysqli->query("INSERT INTO `user`(`hash`, `nickname`, `email`, `verification`)".
+						   "VALUES (".
+						   "'".password_hash($password, PASSWORD_BCRYPT)."',".
+						   "'".$nickname."', ".
+						   "'".$email."', ".
+						   "'".$verification."');")) {
+			
+			if ($this->sendVerificationEmail(HOST, $email, $verification)) {
+				
+				$mysqli->query("COMMIT;");
+				return self::SUCCESS;
+				
+			} else {
+				$mysqli->query("ROLLBACK TO reg_".$nickname.";");
+				$mysqli->query("COMMIT;");
+				
+				return self::EMAIL_UNSENT;
+			}
+			
+		}
+		
+		return self::DB_ERROR;
+		
 	}
 	
-	// set from query result
-	function set_from_qresult($res) {
+	// sending email with verification address
+	private function sendVerificationEmail($verify_host, $email, $verification) {
 		
-		$this->account_id = $res['account_id'];
-		$this->login = $res['login'];
-		$this->email = $res['email'];
-		$this->avatar = $res['avatar'];
-		$this->first_name = $res['first_name'];
-		$this->second_name = $res['second_name'];
-		$this->patronymic = $res['patronymic'];
-		$this->gender = $res['gender'];
-		$this->biography = $res['biography'];
-		$this->phone = $res['phone'];
-		$this->birthday = $res['birthday'];
-		$this->register_date = $res['register_date'];
+		$subject = 'Registration | Verification';
+		$message = '
+		 
+			Дякуємо за реєстрацію!
+			Ваш акаунт було створено, після активації Ви зможете увійти використовуючи наступні дані:
+			
+			 
+			Активація акаунту:
+			'.HOST.'/verify.php?email='.$email.'&verification='.$verification.'
+		 
+		';
 		
+		mail($email, $subject, $message);
+		
+	}
+	
+	// verifying user
+	public function verify($verification) {
+	
+	}fgh
+	
+	// getting public data of user by id
+	public function getUser($user_id) {
+		
+		require "env.php";
+		$mysqli = include DB_CONNECT;
+		
+		if (!$mysqli)
+			return self::DB_ERROR;
+		
+		if ($result = $mysqli->query("SELECT `user`.* FROM `user` WHERE `user`.`user_id`='".$user_id."';")) {
+			if ($res = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+				
+				$this->logout();
+				
+				$this->user_id = $res['user_id'];
+				$this->nickname = $res['nickname'];
+				
+				return self::SUCCESS;
+				
+			}
+		}
+		
+		return self::NO_USER;
+		
+	}
+	
+	public function editPassword($password) {
+	}
+	
+	public function editNickname($nickname) {
+	}
+	
+	public function editEmail($email) {
 	}
 	
 	// reloads the account from database
