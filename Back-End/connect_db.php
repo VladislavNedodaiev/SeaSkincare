1d7ae18fbc4a7f4ba396a1df8cea82c9c1945cc6
Vladislav Nedodaiev <vladislavnedodaiev@gmail.com>
2019-11-17 21:34:47 +0200

    Updated user class: +login function +logout function +constants +getUser function

--- Back-End/connect_db.php
+++ Back-End/connect_db.php
@@ -1,8 +1,5 @@
 <?php
 
-if (!isset($_SESSION))
-	session_start();
-
 $host = "127.0.0.1";
 $user = "root";
 $pswd = "";
@@ -11,10 +8,11 @@ $db = "vacationskin";
 $mysqli = new mysqli($host, $user, $pswd, $db);
 
 if ($mysqli->connect_errno) {
-	$_SESSION['msg']['type'] = "alert-danger";
-	$_SESSION['msg']['text'] = "Не вийшло підключитися до бази даних: ".mysqli_connect_error();
+	return null;
 }
 
 $mysqli->set_charset('utf8');
+
+return $mysqli;
 
 ?>
