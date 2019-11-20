d7afb111c81e9cdc29be39ae4c55405902b89bb8
Vladislav Nedodaiev <vladislavnedodaiev@gmail.com>
2019-11-20 19:06:03 +0200

    Added User Entity

--- SeaSkincare/Backend/Entities/User.php
+++ SeaSkincare/Backend/Entities/User.php
@@ -2,18 +2,27 @@
 
 namespace SeaSkincare\Backend\Entities;
 
-class User {
+class User
+{
 	
-	private $user_id;
-	private $password;
+	private $id;
+	private $passwordHash;
 	private $nickname;
 	private $email;
-	private $verification
+	private $verificationHash;
 	
-	public function getUserID() {
+	// GET
+	
+	public function getID() {
 		
-		return $this->user_id;
+		return $this->id;
 		
+	}
+	
+	public function getPasswordHash() {
+	
+		return $this->passwordHash;
+	
 	}
 	
 	public function getNickname() {
@@ -25,6 +34,44 @@ class User {
 	public function getEmail() {
 		
 		return $this->email;
+		
+	}
+	
+	public function getVerificationHash() {
+		
+		return $this->verificationHash;
+		
+	}
+	
+	// SET
+	
+	public function setID($id) {
+		
+		return $this->id = $id;
+		
+	}
+	
+	public function setPasswordHash($passwordHash) {
+	
+		return $this->passwordHash = $passwordHash;
+	
+	}
+	
+	public function setNickname($nickname) {
+		
+		return $this->nickname = $nickname;
+		
+	}
+	
+	public function setEmail($email) {
+		
+		return $this->email = $email;
+		
+	}
+	
+	public function setVerificationHash($verificationHash) {
+		
+		return $this->verificationHash = $verificationHash;
 		
 	}
 	
