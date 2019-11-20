cb40f2d9cc4f2c5bb2b74d3423fb3c4241c4bce1
Vladislav Nedodaiev <vladislavnedodaiev@gmail.com>
2019-11-20 20:15:23 +0200

    Add: Buoy entity

---
+++ SeaSkincare/Backend/Entities/Buoy.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SeaSkincare\Backend\Entities;
+
+class Buoy
+{
+	
+	private $id;
+	private $fabricationDate;
+	
+	public $subscriptions;
+	public $connections;
+	
+	// GET
+	
+	public function getID() {
+		
+		return $this->id;
+		
+	}
+	
+	public function getFabricationDate() {
+	
+		return $this->fabricationDate;
+	
+	}
+	}
+	
+	// SET
+	
+	public function setID($id) {
+		
+		return $this->id = $id;
+		
+	}
+	
+	public function setFabricationDate($fabricationDate) {
+	
+		return $this->fabricationDate = $fabricationDate;
+	
+	}
+	
+}
+
+?>
