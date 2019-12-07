cff84f316a2f3f90b8e0e485cd0e813045f8ef72
Vladislav Nedodaiev <vladislavnedodaiev@gmail.com>
2019-12-07 14:43:32 +0200

    API ADDED

---
+++ SeaSkincare/Backend/API/Connection/Delete.php
@@ -0,0 +1,22 @@
+<?php
+namespace SeaSkincare\Backend\API\Connection;
+
+include_once '../../Data/DataRepository.php';
+include_once '../../Communication/Response.php';
+
+include_once '../../DTOs/ConnectionDTO.php';
+include_once '../../Services/ConnectionService.php';
+include_once '../../Controllers/ConnectionController.php';
+
+use SeaSkincare\Backend\Controllers\ConnectionController;
+use SeaSkincare\Backend\Communication\Response;
+
+header('Content-Type: text/html; charset=utf-8');
+session_start();
+
+$connectionController = new ConnectionController;
+
+echo json_encode($connectionController->deleteConnection($_POST['connectionID']));
+exit;
+
+?>
