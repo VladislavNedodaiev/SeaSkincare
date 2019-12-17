f274612f716b0b56c8d35d38b0ff84c6c19eba6c
Vladislav Nedodaiev <vladislavnedodaiev@gmail.com>
2019-12-17 19:51:41 +0200

    Time edited

--- SeaSkincare/Backend/API/Subscription/Edit.php
+++ SeaSkincare/Backend/API/Subscription/Edit.php
@@ -15,7 +15,7 @@ header('Content-Type: text/html; charset=utf-8');
 session_start();
 
 $req_dump = print_r($_POST, true);
-$fp = file_put_contents('../../log.txt', date('d.m.Y H:i:s '.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].' POST:').$req_dump.PHP_EOL, FILE_APPEND);
+$fp = file_put_contents('../../log.txt', date('d.m.Y H:i:s ').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].' POST:'.$req_dump.PHP_EOL, FILE_APPEND);
 
 $subscriptionController = new SubscriptionController;
 
