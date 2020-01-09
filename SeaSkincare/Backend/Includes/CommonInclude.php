aa6ad1c9dded99325f0a1f1327fe01110826dcdb
Vladislav Nedodaiev <vladislavnedodaiev@gmail.com>
2020-01-10 01:46:24 +0200

    Edit include

--- SeaSkincare/Backend/Includes/CommonInclude.php
+++ SeaSkincare/Backend/Includes/CommonInclude.php
@@ -1,6 +1,8 @@
 <?php
 
 include_once '../../Data/DataRepository.php';
+include_once '../../Controllers/Controller.php';
+include_once '../../Services/Service.php';
 include_once '../../Services/LogService.php';
 include_once '../../Services/MailService.php';
 include_once '../../Communication/Response.php';
